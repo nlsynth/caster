a01eef6f161aab9345183aabbe515fe136f5c163
Yusuke Tabata <tabata.yusuke@gmail.com>
2021-09-12 21:02:41 +0900

    indent counter.

--- transpiler/indenter.go
+++ transpiler/indenter.go
@@ -22,12 +22,55 @@ func (i *indenter) Write(p []byte) (n int, err error) {
 }
 
 func (i *indenter) flush() error {
-	tokens := strings.Split(i.s, "\n")
-	for _, t := range tokens {
-		_, err := i.w.Write([]byte(t + "\n"))
+	lines := strings.Split(i.s, "\n")
+	level := 0
+	for _, line := range lines {
+		line = i.trimHead(line)
+		level = i.preUpdateLevel(level, line)
+		line = i.addIndent(level, line)
+		level = i.postUpdateLevel(level, line)
+		_, err := i.w.Write([]byte(line + "\n"))
 		if err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func (i *indenter) trimHead(s string) string {
+	for strings.HasPrefix(s, " ") {
+		s = s[1:]
+	}
+	return s
+}
+
+func (i *indenter) addIndent(d int, s string) string {
+	for c := 0; c < d; c++ {
+		s = " " + s
+	}
+	return s
+}
+
+func (i *indenter) preUpdateLevel(d int, line string) int {
+	if strings.HasPrefix(line, "module") ||
+		strings.HasPrefix(line, "endmodule") {
+		return 0
+	}
+	if strings.HasPrefix(line, "end") {
+		return d - 1
+	}
+	return d
+}
+
+func (i *indenter) postUpdateLevel(d int, line string) int {
+	if strings.HasPrefix(line, "module") {
+		return 1
+	}
+	if strings.HasPrefix(line, "case") {
+		return d + 1
+	}
+	if strings.HasSuffix(line, "begin") {
+		return d + 1
+	}
+	return d
 }
